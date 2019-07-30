0b8791c9b756efa00a641878838741e3d2b7cf83
工作 <ghxuan@outlook.com>
2019-07-30 10:39:07 +0800

    添加边框

--- customControls/mainWidget.py
+++ customControls/mainWidget.py
@@ -3,14 +3,19 @@ from PySide2.QtCore import Qt
 from PySide2.QtGui import QPaintEvent, QPainter, QPen
 
 from customControls.boardWidget import BoardWidget
-from customControls.rootPieces import RootPieces
+from customControls.rootPieces import RootPieces, FWidget
 from customControls.pieces import Car, Horse, Elephant, Bodyguard, General, Gun, Soldier
 
 
 class MainWidget(QWidget):
     def __init__(self, *args, **kwargs):
         super(MainWidget, self).__init__(*args, **kwargs)
+        self.cur = None
         self.length = 30
+        self.diam = self.length * 2
+        self.area = (self.length * 5 / 6) ** 2
+        self.center = self.diam * 5
+        self.first = False
         self.setMouseTracking(True)
         self.base_pieces = {2940: 'Car', 2880: 'Horse', 2820: 'Elephant', 2760: 'Bodyguard', 2700: 'General',
                             1680: 'Gun', 900: 'Soldier', 1020: 'Soldier', 1140: 'Soldier'}
@@ -61,45 +66,58 @@ class MainWidget(QWidget):
             (270, 180): '', (270, -180): '', (-270, 180): '', (-270, -180): '', (270, 240): '', (270, -240): '',
             (-270, 240): '', (-270, -240): ''
         }
-        self.base_widget = {
-            (30, 0): '', (-30, 0): '', (30, 60): '', (30, -60): '', (-30, 60): '', (-30, -60): '', (30, 120): '',
-            (30, -120): '', (-30, 120): '', (-30, -120): '', (30, 180): '', (30, -180): '', (-30, 180): '',
-            (-30, -180): '', (30, 240): '', (30, -240): '', (-30, 240): '', (-30, -240): '', (90, 0): '', (-90, 0): '',
-            (90, 60): '', (90, -60): '', (-90, 60): '', (-90, -60): '', (90, 120): '', (90, -120): '', (-90, 120): '',
-            (-90, -120): '', (90, 180): '', (90, -180): '', (-90, 180): '', (-90, -180): '', (90, 240): '',
-            (90, -240): '', (-90, 240): '', (-90, -240): '', (150, 0): '', (-150, 0): '', (150, 60): '', (150, -60): '',
-            (-150, 60): '', (-150, -60): '', (150, 120): '', (150, -120): '', (-150, 120): '', (-150, -120): '',
-            (150, 180): '', (150, -180): '', (-150, 180): '', (-150, -180): '', (150, 240): '', (150, -240): '',
-            (-150, 240): '', (-150, -240): '', (210, 0): '', (-210, 0): '', (210, 60): '', (210, -60): '',
-            (-210, 60): '', (-210, -60): '', (210, 120): '', (210, -120): '', (-210, 120): '', (-210, -120): '',
-            (210, 180): '', (210, -180): '', (-210, 180): '', (-210, -180): '', (210, 240): '', (210, -240): '',
-            (-210, 240): '', (-210, -240): '', (270, 0): '', (-270, 0): '', (270, 60): '', (270, -60): '',
-            (-270, 60): '', (-270, -60): '', (270, 120): '', (270, -120): '', (-270, 120): '', (-270, -120): '',
-            (270, 180): '', (270, -180): '', (-270, 180): '', (-270, -180): '', (270, 240): '', (270, -240): '',
-            (-270, 240): '', (-270, -240): ''
-        }
+        # widget to point , point to widget
+        self.wtp, self.ptw = {}, {}
         self.set_pieces()
 
     def set_pieces(self):
+        self.can.clear()
         for x, y in self.base.keys():
-            press = RootPieces(self, pos=(x, y), length=self.length)
-            self.base_widget[x, y] = press
+            press = FWidget(self, pos=(x, y), length=self.length)
+            self.ptw[x, y] = press
+            self.wtp[press] = (x, y)
             val = abs(x) * 10 + abs(y)
             if val not in self.base_pieces:
                 self.base[x, y] = ''
             else:
                 piece = eval(f'{self.base_pieces[val]}(self, pos=(x, y), length=self.length)')
                 if x < 0:
+                    self.can.add((x, y))
                     piece.same = True
                 else:
                     piece.same = False
                 self.base[x, y] = piece
 
+    def get_exist(self, x, y):
+        x, y = int(x - self.center), int(y - self.center)
+        k, v = x % self.diam, y % self.diam
+        # print([(x - k - self.length, y - v),
+        #        (x - k + self.length + self.diam, y - v),
+        #        (x - k - self.length, y - v + self.diam),
+        #        (x - k + self.length + self.diam, y - v + self.diam)])
+        for c, t in [(k, v), (k - self.diam, v), (k, v - self.diam), (k - self.diam, v - self.diam)]:
+            if c ** 2 + t ** 2 <= self.area:
+                return x - self.length - c, y - t
+
     def mouseMoveEvent(self, event):
-        # print(event)
-        # print(dir(event))
+        x, y = event.pos().toTuple()
+        temp = self.get_exist(x, y)
+        # print(x, y, temp)
+        if not (temp and temp in self.can):
+            if hasattr(self.cur, 'animation'):
+                self.cur.animation.stop()
+                self.cur.opacity.setOpacity(0)
+            return
+        x, y = temp
+        self.cur = self.ptw[x, y]
+        # print(cur, x, y)
+        self.cur.animation.start()
         pass
 
     def mousePressEvent(self, event):
-        print(dir(event))
-        print(event.pos())
+        x, y = event.pos().toTuple()
+        temp = self.get_exist(x, y)
+        if not (temp and temp in self.can):
+            return
+        x, y = temp
+        cur = self.ptw[x, y]
--- customControls/rootPieces.py
+++ customControls/rootPieces.py
@@ -41,7 +41,7 @@ class RootPieces(QWidget):
 
 
 class FWidget(QWidget):
-    def __init__(self, *args, length=30, **kwargs):
+    def __init__(self, *args, pos=(0, 0), length=30, **kwargs):
         super(FWidget, self).__init__(*args, **kwargs)
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowMinimizeButtonHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
@@ -51,6 +51,8 @@ class FWidget(QWidget):
         self.resize(self.lengths[12], self.lengths[12])
         self.center = length * 10, length * 9
         self.opacity = QGraphicsOpacityEffect(self)
+        x, y = pos
+        self.move(x, y)
         self.setGraphicsEffect(self.opacity)
         self.opacity.setOpacity(0)
         self.animation = QPropertyAnimation(self.opacity, b'opacity')
@@ -61,6 +63,15 @@ class FWidget(QWidget):
         # self.animation.setKeyValueAt(0.75, 0.67)
         self.animation.setEndValue(0)
         self.animation.setLoopCount(-1)
+
+    def pos(self):
+        x, y = super(FWidget, self).pos().toTuple()
+        return int(x - self.center[0]), int(y - self.center[1])
+
+    def move(self, *arg__1):
+        x, y = arg__1
+        super(FWidget, self).move(x + self.center[0], y + self.center[1])
+        pass
 
     def paintEvent(self, event: QPaintEvent):
         painter = QPainter()
